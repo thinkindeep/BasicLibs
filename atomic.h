ca04c005991037a397571b11143dd3ca6f668490
thinkindeep <chunxin.ycx@gmail.com>
2015-03-24 15:39:22 +0800

    add atomic ops on bitwise to atomic.h

--- atomic.h
+++ atomic.h
@@ -1,3 +1,9 @@
+/*
+ * The atomic header file includes atomic ops on integer and bitwise, 
+ * you could find them in linux kernel.
+ * chunxin.ycx@gmail.com
+ */
+
 #ifndef __ATOMIC_H__
 #define __ATOMIC_H__
 
@@ -8,10 +14,12 @@ XXX_CPP_START
 
 #define LOCK_PREFIX "lock;"
 
+/* The atomic operations on integer */
 #if __WORDSZIE == 64
 typedef volatile int64_t self_atomic_t;
 
 #define self_atomic_set(v, i) (v) = (i);
+#define ATOMIC_INIT(i) (i)
 
 static inline void self_atomic_add(int64_t i, self_atomic_t *v)
 {
@@ -173,5 +181,133 @@ static inline int32_t self_atomic_read(const self_atomic_t *v)
 }
 #endif
 
-XXX_CPP_END
+/* The atomic operations on bitwise */
+
+
+#XXX_CPP_END
+f __GNUC__ < 4 || (__GNUC__ == 4 && __GNUC_MINOR__ < 1)
+#define BITOP_ADDR(x) "=m" (*(volatile long *) (x))
+#else
+#define BITOP_ADDR(x) "+m" (*(volatile long *) (x))
+#endif
+
+#define ADDR BITOP_ADDR(addr)
+
+#define IS_IMMEDIATE(n)    (__builtin_constant_p(nr))
+#define CONST_MASK_ADDR(nr, addr)    BITOP_ADDR((void *)(addr) + ((nr) >>3))
+#define CONST_MASK(nr)    (1 << ((nr) & 7))
+
+static inline void set_bit(unsigned int nr, volatile unsigned long *addr)
+{
+    if (IS_IMMEDIATE(nr)) {
+        asm volatile(LOCK_PREFIX "orb %1, %0"
+                    : CONST_MASK_ADDR(nr, addr)
+                    : "iq" ((u8)CONST_MASK(nr))
+                    : "memory");
+    } else {
+        asm volatile(LOCK_PREFIX "bts %1, %0"
+                    : BITOP_ADDR(addr) : "Ir" (nr) : "memory");
+    }
+}
+
+static inline void clear_bit(int nr, volatile unsigned long *addr)
+{
+    if (IS_IMMEDIATE(nr)) {
+        asm volatile(LOCK_PREFIX "andb %1, %0"
+                    : CONST_MASK_ADDR(nr, addr)
+                    : "iq" ((u8) ~CONST_MASK(nr)));
+    } else {
+        asm volatile(LOCK_PREFIX "btr %1, %0"
+                    : BITOP_ADDR(addr)
+                    : "Ir" (nr));
+    }
+}
+
+static inline void change_bit(int nr, volatile unsigned long *addr)
+{
+    if (IS_IMMEDIATE(nr)) {
+        asm volatile(LOCK_PREFIX "xorb %1,%0"
+            : CONST_MASK_ADDR(nr, addr)
+            : "iq" ((u8)CONST_MASK(nr)));
+    } else {
+        asm volatile(LOCK_PREFIX "btc %1,%0"
+            : BITOP_ADDR(addr)
+            : "Ir" (nr));
+    }
+}
+
+static inline int test_and_set_bit(int nr, volatile unsigned long *addr)
+{
+    int oldbit;
+
+    asm volatile(LOCK_PREFIX "bts %2,%1\n\t"
+             "sbb %0,%0" : "=r" (oldbit), ADDR : "Ir" (nr) : "memory");
+
+    return oldbit;
+}
+
+static inline int test_and_clear_bit(int nr, volatile unsigned long *addr)
+{
+    int oldbit;
+
+    asm volatile(LOCK_PREFIX "btr %2,%1\n\t"
+             "sbb %0,%0"
+             : "=r" (oldbit), ADDR : "Ir" (nr) : "memory");
+
+    return oldbit;
+}
+
+static inline int test_and_change_bit(int nr, volatile unsigned long *addr)
+{
+    int oldbit;
+
+    asm volatile(LOCK_PREFIX "btc %2,%1\n\t"
+             "sbb %0,%0"
+             : "=r" (oldbit), ADDR : "Ir" (nr) : "memory");
+
+    return oldbit;
+}
+
+/**
+ * ffs - find first set bit in word
+ * @word: The word to search
+ *
+ * Undefined if no bit exists, so code should check against 0 first.
+ */
+static inline unsigned long ffs(unsigned long word)
+{
+	asm("bsf %1,%0"
+			: "=r" (word)
+        : "rm" (word));
+    return word;
+}
+
+/**
+ * ffz - find first zero bit in word
+ * @word: The word to search
+ *
+ * Undefined if no zero exists, so code should check against ~0UL first.
+ */
+static inline unsigned long ffz(unsigned long word)
+{
+	asm("bsf %1,%0"
+			: "=r" (word)
+			: "r" (~word));
+    return word;
+}
+
+/*
+ * fls: find last set bit in word
+ * @word: The word to search
+ *
+ * Undefined if no set bit exists, so code should check against 0 first.
+ */
+static inline unsigned long fls(unsigned long word)
+{
+	asm("bsr %1,%0"
+			: "=r" (word)
+			: "rm" (word));
+    return word;
+}
+
 #endif
